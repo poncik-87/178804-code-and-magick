7784fcd2bc749b0941d88a73b3994e506fa94b45
poncik-87 <komarov.aleksey@gmail.com>
2016-08-12 21:17:28 +0300

    Исправлены замечания pr

--- src/form.js
+++ src/form.js
@@ -1,8 +1,29 @@
 'use strict';
 
 window.form = (function() {
+  /**
+   *@constant
+   *@type {string}
+   */
+  var INVISIBLE = 'invisible';
+  /**
+   *@constant
+   *@type {number}
+   */
+  var THRESHOLD_MARK = 3;
+    
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
+    
+  var form = document.querySelector('.review-form');
+  var reviewName = document.querySelector('#review-name');
+  var reviewText = document.querySelector('#review-text');
+  var reviewFieldsName = document.querySelector('.review-fields-name');
+  var reviewFieldsText = document.querySelector('.review-fields-text');
+  var reviewFieldsHeader = document.querySelector('.review-fields');
+  var submitButton = document.querySelector('.review-submit');
+  var reviewMarks = document.querySelectorAll('.review-form-group-mark input[type=\"radio\"]');
+  var currentMark;
 
   var form = {
     onClose: null,
@@ -11,12 +32,12 @@ window.form = (function() {
      * @param {Function} cb
      */
     open: function(cb) {
-      formContainer.classList.remove('invisible');
+      formContainer.classList.remove(INVISIBLE);
       cb();
     },
 
     close: function() {
-      formContainer.classList.add('invisible');
+      formContainer.classList.add(INVISIBLE);
 
       if (typeof this.onClose === 'function') {
         this.onClose();
@@ -24,58 +45,52 @@ window.form = (function() {
     }
   };
 
-
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
     form.close();
   };
+    
+  reviewName.oninput = checkFormRequires;
+  reviewText.oninput = checkFormRequires;
+  for (var i = 0; i < reviewMarks.length; i++) {
+    if (reviewMarks[i].checked) {
+      currentMark = reviewMarks[i].value;
+    }
+
+    reviewMarks[i].onchange = function() {
+      currentMark = this.value;
+      checkFormRequires();
+    };
+  }
+
+  reviewName.required = true;
+  checkFormRequires();
 
   return form;
-})();
+    
+  /**
+  * Реагирование формы на пользовательские действия
+  */
+  function checkFormRequires() {
+    reviewText.required = currentMark < THRESHOLD_MARK;
 
-var reviewName = document.querySelector('#review-name');
-var reviewText = document.querySelector('#review-text');
-var reviewFieldsName = document.querySelector('.review-fields-name');
-var reviewFieldsText = document.querySelector('.review-fields-text');
-var reviewFieldsHeader = document.querySelector('.review-fields');
-var submitButton = document.querySelector('.review-submit');
-var reviewMarks = document.querySelectorAll('.review-form-group-mark input[type=\"radio\"]');
-var currentMark;
+    //скрытие лейблов, если соответствующие поля валидны
+    if (reviewName.validity.valid) {
+      reviewFieldsName.classList.add(INVISIBLE);
+    } else {
+      reviewFieldsName.classList.remove(INVISIBLE);
+    }
+    if (reviewText.validity.valid) {
+      reviewFieldsText.classList.add(INVISIBLE);
+    } else {
+      reviewFieldsText.classList.remove(INVISIBLE);
+    }
+    if (reviewName.validity.valid && reviewText.validity.valid) {
+      reviewFieldsHeader.classList.add(INVISIBLE);
+    } else {
+      reviewFieldsHeader.classList.remove(INVISIBLE);
+    }
 
-var checkFormRequires = function() {
-  reviewText.required = currentMark < 3;
-
-  if (reviewName.value !== '') {
-    reviewFieldsName.classList.add('invisible');
-  } else {
-    reviewFieldsName.classList.remove('invisible');
-  }
-  if (reviewText.value !== '') {
-    reviewFieldsText.classList.add('invisible');
-  } else {
-    reviewFieldsText.classList.remove('invisible');
-  }
-  if (reviewName.value !== '' && reviewText.value !== '') {
-    reviewFieldsHeader.classList.add('invisible');
-  } else {
-    reviewFieldsHeader.classList.remove('invisible');
-  }
-
-  submitButton.disabled = reviewName.value === '' || (reviewText.required && reviewText.value === '');
-};
-
-reviewName.onchange = checkFormRequires;
-reviewText.onchange = checkFormRequires;
-for (var i = 0; i < reviewMarks.length; i++) {
-  if (reviewMarks[i].checked) {
-    currentMark = reviewMarks[i].value;
-  }
-
-  reviewMarks[i].onchange = function() {
-    currentMark = this.value;
-    checkFormRequires();
+    //submitButton.disabled = form.validity.valid;
   };
-}
-
-reviewName.required = true;
-checkFormRequires();
+})();
