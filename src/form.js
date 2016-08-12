9edb93fad2d4c9fd2fd243a1b13668afe318331d
poncik-87 <komarov.aleksey@gmail.com>
2016-08-12 16:24:49 +0300

    Добавлена валидация формы

--- src/form.js
+++ src/form.js
@@ -32,3 +32,46 @@ window.form = (function() {
 
   return form;
 })();
+
+var reviewName = document.querySelector('#review-name');
+var reviewText = document.querySelector('#review-text');
+var reviewFieldsName = document.querySelector('.review-fields-name');
+var reviewFieldsText = document.querySelector('.review-fields-text');
+var reviewFieldsHeader = document.querySelector('.review-fields');
+var submitButton = document.querySelector('.review-submit');
+var reviewMarks = document.querySelectorAll('.review-form-group-mark input[type=\"radio\"]');
+var currentMark;
+
+var checkFormRequires = function() {
+  reviewText.required = currentMark < 3;
+
+  if (reviewName.value !== '') {
+    reviewFieldsName.classList.add('invisible');
+  } else {
+    reviewFieldsName.classList.remove('invisible');
+  }
+  if (reviewText.value !== '') {
+    reviewFieldsText.classList.add('invisible');
+  } else {
+    reviewFieldsText.classList.remove('invisible');
+  }
+  if (reviewName.value !== '' && reviewText.value !== '') {
+    reviewFieldsHeader.classList.add('invisible');
+  } else {
+    reviewFieldsHeader.classList.remove('invisible');
+  }
+
+  submitButton.disabled = reviewName.value === '' || (reviewText.required && reviewText.value === '');
+};
+
+reviewName.onchange = checkFormRequires;
+reviewText.onchange = checkFormRequires;
+for (var i = 0; i < reviewMarks.length; i++) {
+  reviewMarks[i].onchange = function() {
+    currentMark = this.value;
+    checkFormRequires();
+  };
+}
+
+reviewName.required = true;
+checkFormRequires();
