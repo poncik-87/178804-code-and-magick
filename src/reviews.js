a8c13a4d91d934f5f205da9ff2e0775680eca604
poncik-87 <komarov.aleksey@gmail.com>
2016-08-19 15:55:20 +0300

    Отображение отзывов по шаблону в templateÐ

--- src/reviews.js
+++ src/reviews.js
@@ -14,13 +14,65 @@ window.jsonpCall = function(requestAdress, handlerFunc) {
 };
 
 (function() {
+  /**
+   *@constant
+   *@type {number}
+   */
+  var IMAGE_SIZE = 124;
+
   var reviews;
   var address = 'http://localhost:1506/api/reviews?callback=';
+  var templateElement = document.querySelector('#review-template');
+  var elementToClone;
+  var reviewListElement = document.querySelector('.reviews-list');
 
   window.jsonpCall(address, function(data) {
+    var reviewsFilter = document.querySelector('.reviews-filter');
+
+    if ('content' in templateElement) {
+      elementToClone = templateElement.content.querySelector('.review');
+    } else {
+      elementToClone = templateElement.querySelector('.review');
+    }
+
+    reviewsFilter.classList.add('invisible');
+
     reviews = data;
-    console.log(reviews);
+    reviews.forEach(function(review) {
+      createDOMForReview(review);
+    });
+
+    reviewsFilter.classList.remove('invisible');
+    reviewsFilter.classList.remove('invisible');
   });
+
+  /**
+   *Создает разметку для отзыва по template элементу разметки
+   * @param {Object} review
+   */
+  function createDOMForReview(review) {
+    var reviewElement = elementToClone.cloneNode(true);
+    var author = reviewElement.querySelector('.review-author');
+    var rating = reviewElement.querySelector('.review-rating');
+    var text = reviewElement.querySelector('.review-text');
+
+    author.title = review.author.name;
+    text.innerHTML = review.description;
+    rating.style.width *= review.rating;
+
+    var authorImage = new Image();
+
+    authorImage.onload = function(evt) {
+      author.src = evt.target.src;
+      author.width = IMAGE_SIZE;
+      author.height = IMAGE_SIZE;
+    };
+    authorImage.onerror = function() {
+      reviewElement.classList.add('review-load-failure');
+    };
+
+    authorImage.src = review.author.picture;
+
+    reviewListElement.appendChild(reviewElement);
+  }
 })();
-
-
