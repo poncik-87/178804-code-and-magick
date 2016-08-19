831bede61d956ecbce8b4042104a0475808b67d2
poncik-87 <komarov.aleksey@gmail.com>
2016-08-19 10:40:35 +0300

    ДаобалвÐвлено присÐИсправлеДобавлено Ðа вставка создаваемого скрипта в дом Ð

--- src/reviews.js
+++ src/reviews.js
@@ -1,18 +1,23 @@
 'use strict';
 
+/**
+ * @param {string} requestAdress
+ * @param {function} handlerFunc
+ */
 window.jsonpCall = function(requestAdress, handlerFunc) {
   window.__jsonpCallback = handlerFunc;
+  requestAdress += '__jsonpCallback';
 
   var scriptElement = document.createElement('script');
   scriptElement.src = requestAdress;
+  document.body.appendChild(scriptElement);
 };
 
 (function() {
   var reviews;
-  var addres = 'http://localhost:1506/api/reviews?callback=__jsonpCallback';
+  var addres = 'http://localhost:1506/api/reviews?callback=';
   var handler = function(data) {
     reviews = data;
-    console.log(reviews);
   };
 
   window.jsonpCall(addres, handler);
