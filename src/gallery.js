2642344bd5aedef521ef2907875af08499b6fcd5
poncik-87 <komarov.aleksey@gmail.com>
2016-08-26 17:16:42 +0300

    Добавлен модуль превью скриншотов, рабочая версия

---
+++ src/gallery.js
@@ -0,0 +1,89 @@
+'use strict';
+
+define(function() {
+  /**
+   * @param {Array} pictures
+   */
+  function Gallery(pictures) {
+    if (Array.isArray(pictures)) {
+      this.pictures = pictures;
+    } else {
+      this.pictures = pictures;
+    }
+
+    this.activePicture = 0;
+    this.overlayGalleryElement = document.querySelector('.overlay-gallery');
+    this.overlayGalleryControlLeftElement = document.querySelector('.overlay-gallery-control-left');
+    this.overlayGalleryControlRightElement = document.querySelector('.overlay-gallery-control-right');
+    this.previewNumberCurrentElement = document.querySelector('.preview-number-current');
+    this.previewNumberTotalElement = document.querySelector('.preview-number-total');
+    this.overlayGalleryCloseElement = document.querySelector('.overlay-gallery-close');
+    this.overlayGalleryPreviewElement = document.querySelector('.overlay-gallery-preview');
+  }
+
+  /**
+   * @param {number} index
+   */
+  Gallery.prototype.show = function(index) {
+    this.overlayGalleryElement.classList.remove('invisible');
+
+    var self = this;
+
+    this.overlayGalleryCloseElement.onclick = function() {
+      self.hide();
+    };
+
+    this.overlayGalleryControlLeftElement.onclick = function() {
+      if(self.activePicture > 0) {
+        self.setActivePicture(self.activePicture - 1);
+      }
+    };
+
+    this.overlayGalleryControlRightElement.onclick = function() {
+      if(self.activePicture < self.pictures.length - 1) {
+        self.setActivePicture(self.activePicture + 1);
+      }
+    };
+
+    this.setActivePicture(index);
+    this.previewNumberTotalElement.innerHTML = this.pictures.length;
+  };
+
+  Gallery.prototype.hide = function() {
+    this.overlayGalleryElement.classList.add('invisible');
+
+    this.overlayGalleryCloseElement.onclick = null;
+    this.overlayGalleryControlLeftElement = null;
+    this.overlayGalleryControlRightElement = null;
+  };
+
+  /**
+   * @param {number} index
+   */
+  Gallery.prototype.setActivePicture = function(index) {
+    if(index < 0 || index >= this.pictures.length) {
+      return;
+    }
+
+    this.activePicture = index;
+
+    var currentPicture = this.pictures[index];
+
+    var prevImage = this.overlayGalleryPreviewElement.querySelector('img');
+    if(prevImage) {
+      this.overlayGalleryPreviewElement.removeChild(prevImage);
+    }
+
+    var self = this;
+
+    var image = new Image();
+    image.onload = function(evt) {
+      self.overlayGalleryPreviewElement.appendChild(evt.target);
+    };
+    image.src = currentPicture;
+
+    this.previewNumberCurrentElement.innerHTML = index + 1;
+  };
+
+  return Gallery;
+});
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-define(['./form', './game', './reviews'], function(form, Game) {
+define(['./form', './game', './gallery', './reviews'], function(form, Game, Gallery) {
   var game = new Game(document.querySelector('.demo'));
   game.initializeLevelAndStart();
   game.setGameStatus(Game.Verdict.INTRO);
@@ -20,4 +20,22 @@ define(['./form', './game', './reviews'], function(form, Game) {
   form.onClose = function() {
     game.setDeactivated(false);
   };
+
+  var pictures = [];
+  var photogalleryElement = document.querySelector('.photogallery');
+  var photogalleryImageElements = photogalleryElement.querySelectorAll('.photogallery-image img');
+  var i;
+
+  for(i = 0; i < photogalleryImageElements.length; i++) {
+    pictures.push(photogalleryImageElements[i].src);
+  }
+
+  var gallery = new Gallery(pictures);
+
+  for(i = 0; i < photogalleryImageElements.length; i++) {
+    //TODO: нужно передавать правильный индекс
+    photogalleryImageElements[i].onclick = function(evt) {
+      gallery.show(photogalleryImageElements.indexOf(evt.target));
+    };
+  }
 });
